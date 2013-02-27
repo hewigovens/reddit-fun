a65d2708e962fd2ff1505fec404d789cd655a555
hewigovens <hewigovens@gmail.com>
2013-02-27 22:18:07 +0800

    burn rss using lxml

--- index.py
+++ index.py
@@ -2,25 +2,27 @@
 # coding: utf-8
 from gevent import monkey; monkey.patch_all()
 from bottle import route, run, template, redirect, request, response
-from lxml import etree, html
-from readability.readability import Document
+from lxml import etree
+# from readability.readability import Document
 import os
 
 import gevent
 import feedparser
 import StringIO
 import requests
-import PyRSS2Gen
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
+logging.basicConfig(
+    level=logging.DEBUG, format='%(levelname)s - %(asctime)s %(message)s',
+    datefmt='[%b %d %H:%M:%S]')
+
 
 def parse_rss(url):
 
     feed = feedparser.parse(url)
     articles = {}
 
-    #print feed['items'][0]
+    # print feed['items'][0]
 
     for item in feed['items']:
         text = StringIO.StringIO('<html>%s</html>' % item['description'])
@@ -28,41 +30,64 @@ def parse_rss(url):
         a = tree.xpath('//a[text()="[link]"]/@href')
         articles[''.join(a)] = item['title']
 
-    #print articles
+    # print articles
 
     jobs = [gevent.spawn(fetch_article, articles[url], url) for url in articles.keys()]
     gevent.joinall(jobs, timeout=8)
 
-    full_items = []
+    full_rss = etree.Element('rss', attrib={'version': '2.0'})
+    channel = etree.SubElement(full_rss, 'channel')
+
+    title = etree.SubElement(channel, 'title')
+    title.text = feed['channel']['title']
+
+    link = etree.SubElement(channel, 'link')
+    link.text = feed['channel']['link']
+
+    description = etree.SubElement(channel, 'description')
+    description.text = feed['channel']['description']
+
     for index in xrange(len(jobs)):
         if jobs[index].value:
-            item = feed['items'][index]
-            full_items.append(PyRSS2Gen.RSSItem(title=item['title'],
-                                                link=item['link'],
-                                                description=jobs[index].value,
-                                                guid=PyRSS2Gen.Guid(item['link']),
-                                                pubDate=item['published']))
 
-    full_feed = PyRSS2Gen.RSS2(title=feed['channel']['title'],
-                               link=feed['channel']['link'],
-                               description=feed['channel']['description'],
-                               items=full_items)
+            i = feed['items'][index]
+            item = etree.Element('item')
+            title = etree.Element('title')
+            title.text = i['title']
 
-    #full_feed.write_xml(open('1.xml','w'))
-    return full_feed.to_xml(encoding="utf-8")
+            link = etree.Element('link')
+            link.text = i['link']
 
-def fetch_article(title,url):
+            guid = etree.Element('guid')
+            guid.text = i['link']
+
+            description = etree.Element('description')
+            description.text = r'<![CDATA[ %s ]]' % jobs[index].value
+
+            item.append(title)
+            item.append(link)
+            item.append(guid)
+            item.append(description)
+
+            channel.append(item)
+
+    root = etree.ElementTree(full_rss)
+    return etree.tostring(root)
+
+
+def fetch_article(title, url):
     try:
         logging.info('start fetch %s: %s' % (title, url))
-        headers={'User-Agent':'happy happy bot 0.1 by /u/hewigovens'}
+        headers = {'User-Agent': 'happy happy bot 0.1 by /u/hewigovens'}
         data = requests.get(url, timeout=5, headers=headers).text
 
-        readable_article = Document(data)
-        readable_summary = readable_article.summary()
-        readable_summary_html = html.fromstring(readable_summary)
-        readable_text = readable_summary_html.text_content()
+        return data
+        # readable_article = Document(data)
+        # readable_summary = readable_article.summary()
+        # readable_summary_html = html.fromstring(readable_summary)
+        # readable_text = readable_summary_html.text_content()
 
-        return readable_text
+        # return readable_text
     except Exception as e:
         logging.error(e.message)
         return None
@@ -72,6 +97,7 @@ def fetch_article(title,url):
 def index():
     return template('templates/index')
 
+
 @route('/add/')
 def add_url():
     # url = request.forms.get('url')
@@ -80,11 +106,12 @@ def add_url():
         topic = r'programming'
     return redirect('/reddit/%s' % topic)
 
+
 @route('/reddit/:topic')
 def full_topic(topic):
     if topic:
         url = (r'http://www.reddit.com/r/%s/.rss' % topic)
-        response.set_header('Content-Type','application/xml')
+        response.set_header('Content-Type', 'application/xml')
         return parse_rss(url)
 
 
